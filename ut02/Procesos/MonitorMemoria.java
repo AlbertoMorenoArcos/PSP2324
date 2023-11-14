97bde5302ec01a1ef1f8d0e37ec2ec5795537893
Alberto Moreno Arcos <albertomorenoarcos@gmail.com>
2023-11-14 13:22:21 +0100

    .

--- ut02/Procesos/MonitorMemoria.java
+++ ut02/Procesos/MonitorMemoria.java
@@ -1,10 +1,8 @@
 package ut02.Procesos;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
