e870fd606321b9e87160f77f77684dab67019201
Alberto Moreno Arcos <albertomorenoarcos@gmail.com>
2023-11-23 20:32:18 +0100

    carrera mejorada

--- ut02/Threads/Carrera/Principal.java
+++ ut02/Threads/Carrera/Principal.java
@@ -1,13 +1,17 @@
 package Carrera;
 
+import java.util.Scanner;
+
 public class Principal {
 
     private static final int TOTAL_CARRERA = 100;
     private static final int Corredores = 10;
     private static final int Hilos = 10;
     private static final int START_DORSAL = 1;
+    private static final int CUENTA_ATRAS = 0;
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         Object salida = new Object();
         Corredor[] Arraycorredores = new Corredor[Corredores];
         for (int i = 0; i < Arraycorredores.length; i++) {
@@ -19,10 +23,19 @@ public class Principal {
             Arraythreads[i] = new Thread(Arraycorredores[i]);
         }
 
+
         System.out.println("La carrera va a comenzar");
         for (int i = 0; i < Arraythreads.length; i++) {
             Arraythreads[i].start();
         }
+        System.out.println("Pulsa para empezar la carrera.");
+        sc.nextLine();
+        sc.close();
+        for(int i = 10; i>=CUENTA_ATRAS; i--){
+            System.out.println(i + "!!!!");
+        }
+        System.out.println("SALIDA!!!!!!!");
+        
         synchronized (salida) {
             try {
                 salida.notifyAll();
@@ -33,6 +46,7 @@ public class Principal {
         for (int i = 0; i < Arraythreads.length; i++) {
             try {
                 Arraythreads[i].join();
+
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
