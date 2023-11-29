6c40419700e5004390267fcfd4b1760f90bb0e8c
Alberto Moreno Arcos <albertomorenoarcos@gmail.com>
2023-11-29 13:08:21 +0100

    .

--- ut02/Threads/CarreraEjercicio4/Carrera.java
+++ ut02/Threads/CarreraEjercicio4/Carrera.java
@@ -2,37 +2,72 @@ package CarreraEjercicio4;
 
 import java.util.ArrayList;
 
-/**
- * CarreraNotifyAll
- */
 public class Carrera {
-
     public static void main(String[] args) {
         Object salida = new Object();
         Object llegada = new Object();
         ArrayList<Thread> lista = new ArrayList<>();
+
+        // Crear y empezar los hilos de los corredores
         for (int i = 0; i < Corredor.NUM_CORREDORES; i++) {
             Thread thread = new Thread(new Corredor(i + 1, salida, llegada));
             lista.add(thread);
             thread.start();
         }
+
+        // Esperar un tiempo antes de comenzar la carrera
         try {
             Thread.sleep(Corredor.TIEMPO_ESPERA);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
+        // Notificar a todos los corredores que la carrera ha comenzado
         synchronized (salida) {
             salida.notifyAll();
         }
-        synchronized(llegada) {
-            try{
+
+        // Esperar hasta que todos los corredores hayan llegado a la meta
+        synchronized (llegada) {
+            try {
                 llegada.wait();
-            } catch (InterruptedException e) {}            
+            } catch (InterruptedException e) {
+                // Manejar excepciones de interrupción
+            }
         }
-        for(Thread thread : lista){
-            if(thread.isAlive()){
+
+        // Interrumpir cualquier corredor que aún esté en ejecución
+        for (Thread thread : lista) {
+            if (thread.isAlive()) {
                 thread.interrupt();
             }
         }
     }
 }
+
+/*
+ * Objetos compartidos (salida y llegada): Se crean dos objetos Object
+ * compartidos para coordinar la salida y llegada de los corredores.
+ * 
+ * Lista de hilos (lista): Se crea una lista para almacenar los hilos de los
+ * corredores.
+ * 
+ * Creación y ejecución de hilos: Se utiliza un bucle for para crear e iniciar
+ * los hilos de los corredores.
+ * 
+ * Espera antes de la carrera: Se espera un tiempo definido
+ * (Corredor.TIEMPO_ESPERA) antes de notificar a los corredores
+ * para simular cierto período de preparación antes de la carrera.
+ * 
+ * Notificación de salida: Se utiliza synchronized (salida) para garantizar que
+ * la notificación (salida.notifyAll())
+ * se realice de manera segura, informando a todos los corredores que la carrera
+ * ha comenzado.
+ * 
+ * Espera hasta la llegada: Se utiliza synchronized (llegada) y llegada.wait()
+ * para que el hilo principal espere
+ * hasta que todos los corredores hayan llegado a la meta.
+ * 
+ * Interrupción de corredores: Se itera sobre la lista de hilos y se interrumpen
+ * los que aún estén en ejecución.
+ */
--- ut02/Threads/CarreraEjercicio4/Corredor.java
+++ ut02/Threads/CarreraEjercicio4/Corredor.java
@@ -1,11 +1,12 @@
 package CarreraEjercicio4;
+
 public class Corredor implements Runnable {
     int dorsal;
     int distanciaRecorrida;
     Long tiempo = null;
     static final int DISTANCIA_TOTAL = 100, INTERVALO = 10, NUM_CORREDORES = 10;
     static final long TIEMPO_ESPERA = 500;
-    private static final String MSG_PERDEDOR = "Soy el perdedor (nº%d) y me he quedado en en kilómetro %d. Tiempo: %d milisegundos.\n";
+    private static final String MSG_PERDEDOR = "Soy el perdedor (nº%d) y me he quedado en el kilómetro %d. Tiempo: %d milisegundos.\n";
     Object salida;
     Object llegada;
 
@@ -16,28 +17,65 @@ public class Corredor implements Runnable {
         this.distanciaRecorrida = 0;
     }
 
-    public void run(){
+    public void run() {
         long tiempoLlegada = 0;
         long tiempoInicio = 0;
-        try{
+
+        try {
+            // Espera la señal de salida
             synchronized (salida) {
                 salida.wait();
             }
+
             tiempoInicio = System.currentTimeMillis();
-            while(distanciaRecorrida < DISTANCIA_TOTAL){
-                distanciaRecorrida += (int)(Math.random() * INTERVALO);
+
+            // Simula el recorrido del corredor
+            while (distanciaRecorrida < DISTANCIA_TOTAL) {
+                distanciaRecorrida += (int) (Math.random() * INTERVALO);
                 Thread.sleep(TIEMPO_ESPERA);
             }
+
             tiempoLlegada = System.currentTimeMillis();
+
+            // Notifica que ha llegado a la meta
             synchronized (llegada) {
                 llegada.notify();
             }
+
             tiempo = tiempoLlegada - tiempoInicio;
-            System.out.printf("Soy el corredor nº %d y he llegado a la meta en %d milisegundos, y he recorrido %d kilómetros.\n", dorsal, tiempo, distanciaRecorrida);
+            System.out.printf(
+                    "Soy el corredor nº %d y he llegado a la meta en %d milisegundos, y he recorrido %d kilómetros.\n",
+                    dorsal, tiempo, distanciaRecorrida);
         } catch (InterruptedException e) {
+            // El corredor ha sido interrumpido, muestra un mensaje de perdedor
             tiempoLlegada = System.currentTimeMillis();
             tiempo = tiempoLlegada - tiempoInicio;
             System.out.printf(MSG_PERDEDOR, dorsal, distanciaRecorrida, tiempo);
         }
     }
 }
+/*
+ * Método run(): Este método implementa la lógica principal de un corredor.
+ * 
+ * Espera la señal de salida: El corredor sincroniza en el objeto salida y
+ * espera (salida.wait()) hasta que recibe la señal para empezar la carrera.
+ * 
+ * Simulación del recorrido: Utiliza un bucle while para simular el recorrido
+ * del corredor. En cada iteración, se avanza una distancia aleatoria y se
+ * espera un tiempo (Thread.sleep(TIEMPO_ESPERA)) para simular la velocidad del
+ * corredor.
+ * 
+ * Notificación de llegada: Cuando el corredor alcanza la distancia total,
+ * notifica a través del objeto llegada que ha llegado a la meta
+ * (llegada.notify()).
+ * 
+ * Cálculo del tiempo: Calcula el tiempo total que tardó en llegar a la meta.
+ * 
+ * Manejo de interrupciones: Si el corredor es interrumpido (por ejemplo, si la
+ * carrera se cancela), muestra un mensaje indicando que es el perdedor y en qué
+ * kilómetro se quedó.
+ * 
+ * Este código simula de manera efectiva una carrera con corredores y utiliza la
+ * sincronización con wait() y notifyAll() para coordinar el inicio y
+ * finalización de la carrera de manera segura en un entorno multi-hilo.
+ */
