91187442efb0e076735cca2c7b06437802ab7f4d
Alberto Moreno Arcos <albertomorenoarcos@gmail.com>
2023-11-29 13:58:53 +0100

    ..

--- ut01/01/punteros.c
+++ ut01/01/punteros.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 void suma_dos(int *x, int *y, int *z)
 {
-*x=*x+2;
-*y=*y+2;
-*z=*z+2;
+    *x = *x + 2;
+    *y = *y + 2;
+    *z = *z + 2;
 }
-void main(void){
-int x;
-printf("Introduzca tres numeros:");
-scanf("%d %d %d", &x, &y, &z);
-suma_dos (&x, &y, &z);
-printf("%d %d %d", x, y, z);// qué imprimirá??
-} 
+void main(void)
+{
+    int x;
+    int y;
+    int z;
+    printf("Introduzca tres numeros:\n");
+    scanf("%d %d %d", &x, &y, &z);
+    suma_dos(&x, &y, &z);
+    printf("%d %d %d", x, y, z); // qué imprimirá??
+}
