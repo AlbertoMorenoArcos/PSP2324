e12c873fe9a1e32822310a517057c15986d36a08
Alberto Moreno Arcos <albertomorenoarcos@gmail.com>
2023-12-13 17:54:07 +0100

    .

---
+++ PSP_2a_Evaluacion/ut03/udp/ClienteUDP.java
@@ -0,0 +1,35 @@
+package PSP_2a_Evaluacion.ut03.udp;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+public class ClienteUDP {
+
+    public static void main(String[] args) {
+        try {
+            DatagramSocket ds = new DatagramSocket();
+            byte buffer[] = "Hola mundo\n".getBytes();
+            String ip = "127.0.0.1";
+            // ds.setBroadcast(true);
+            DatagramPacket p = new DatagramPacket(
+                    buffer,
+                    buffer.length,
+                    InetAddress.getByName(ip),
+                    4321);
+
+            ds.send(p);
+            ds.close();
+        } catch (SocketException e) {
+            e.printStackTrace();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
